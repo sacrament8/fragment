ca2ef22cbc338ef5be23a4268296888bd6cc6459
sacrament <xkinked.tailx@gmail.com>
2019-08-21 21:22:06 +0900

    ログインユーザーがタイトルロゴをクリック時users.showにリダイレクトするように

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPageController extends Controller
 {
     public function index()
     {
+        if (Auth::check()) {
+            $user = Auth::user();
+            return redirect()->route('users.show', $user->id);
+        }
         return view('staticpages.index');
     }
 }
