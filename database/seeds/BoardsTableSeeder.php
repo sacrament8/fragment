994548d3f53503c3da259b51f916650e8c9e257e
sacrament <xkinked.tailx@gmail.com>
2019-08-23 02:13:09 +0900

    Boardsテーブル用のSeederの作成

---
+++ database/seeds/BoardsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Board;
+use App\User;
+
+class BoardsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($user_id = 1; $user_id <= 101; $user_id++) {
+            for ($i = 1; $i <= 3; $i++) {
+                Board::create([
+                    'title' => User::find($user_id)->name . 'が立てたスレッド' . $i,
+                    'content' => User::find($user_id)->name . 'が立てたスレッドです、自由に書き込んでください。',
+                    'user_id' => $user_id,
+                ]);
+            }
+        }
+    }
+}
